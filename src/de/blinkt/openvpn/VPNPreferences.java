626f28210e1ddfd7a467ce85794ac0fc91a0858d
Arne Schwabe <arne@rfc2549.org>
2012-07-16 22:48:23 +0200

    Start work on clear setting for file dialog Add a quick start quide stating the obvious (*sigh*)

---
+++ src/de/blinkt/openvpn/VPNPreferences.java
@@ -0,0 +1,55 @@
+package de.blinkt.openvpn;
+
+import java.util.List;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+
+public class VPNPreferences extends PreferenceActivity {
+
+	private String mProfileUUID;
+
+	public VPNPreferences() {
+		super();
+	
+	}
+	
+
+
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+	};
+	
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		mProfileUUID = getIntent().getStringExtra(getPackageName() + ".profileUUID");
+		VpnProfile profile = ProfileManager.get(this,mProfileUUID);
+		if(profile!=null) {
+			setTitle(getString(R.string.edit_profile_title, profile.getName()));
+		}
+		super.onCreate(savedInstanceState);
+	}
+	
+	
+	@Override
+	public void onBuildHeaders(List<Header> target) {
+		loadHeadersFromResource(R.xml.vpn_headers, target); 
+		for (Header header : target) {
+			if(header.fragmentArguments==null)
+				header.fragmentArguments = new Bundle();
+			header.fragmentArguments.putString(getPackageName() + ".profileUUID",mProfileUUID);
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		setResult(RESULT_OK, getIntent());
+		super.onBackPressed();
+	}
+}
+
