d5094abbcf7c95f3efa171ad7db02de25c7779f6
Arne Schwabe <arne@rfc2549.org>
2012-12-26 04:57:06 +0100

    Backout changeset 6dbb27f76cd286bd76a9f2be993e0d2241167684

---
+++ fetchtranslations.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+
+if [ "$ICSCROWDAPIKEY" != "" ]
+then
+	echo "Generating new translation archives"
+	fetch -q -1 -o - http://api.crowdin.net/api/project/ics-openvpn/export?key=$ICSCROWDAPIKEY
+fi
+
+echo "Fetch translation archive"
+fetch -q http://crowdin.net/download/project/ics-openvpn.zip
+
+langtoinclude="de cs ko et fr he ru"
+
+for lang in $langtoinclude
+do
+    tar xfv ics-openvpn.zip /res/values-$lang/
+done
+
+# Chinese language require zh-CN and zh-TW
+
+for lang in "zh-CN"
+do
+	if [ $lang = "zh-CN" ] ; then
+		rlang="zh-rCN"
+	fi
+	echo "Fetch archive for $lang"
+	fetch http://crowdin.net/download/project/ics-openvpn/$lang.zip
+	tar -xv -C res/values-$rlang/ --strip-components 3 -f $lang.zip
+done
---
+++ src/de/blinkt/openvpn/OpenVPN.java
@@ -0,0 +1,196 @@
+package de.blinkt.openvpn;
+
+import java.util.LinkedList;
+import java.util.Vector;
+
+import android.content.Context;
+import android.os.Build;
+
+public class OpenVPN {
+	
+
+	public static LinkedList<LogItem> logbuffer;
+	
+	private static Vector<LogListener> logListener;
+	private static Vector<StateListener> stateListener;
+	private static String[] mBconfig;
+
+	private static String mLaststatemsg;
+
+	private static String mLaststate;
+	
+	static {
+		logbuffer  = new LinkedList<LogItem>();
+		logListener = new Vector<OpenVPN.LogListener>();
+		stateListener = new Vector<OpenVPN.StateListener>();
+		logInformation();
+	}
+	
+	static class LogItem {
+		public static final int ERROR = 1;
+		public static final int INFO = 2;
+		public static final int VERBOSE = 3;
+
+		private Object [] mArgs = null;
+		private String mMessage = null;
+		private int mRessourceId;
+		// Default log priority
+		int mLevel = INFO;
+		
+		public LogItem(int ressourceId, Object[] args) {
+		 mRessourceId = ressourceId;
+		 mArgs = args;
+		}
+
+		
+		public LogItem(int loglevel,int ressourceId, Object[] args) {
+			 mRessourceId = ressourceId;
+			 mArgs = args;
+			 mLevel = loglevel;
+			}
+
+		
+		public LogItem(String message) {
+			mMessage = message;
+		}
+
+		public LogItem(int loglevel, String msg) {
+			mLevel = loglevel;
+			mMessage = msg;
+		}
+
+
+		public LogItem(int loglevel, int ressourceId) {
+			mRessourceId =ressourceId;
+			mLevel = loglevel;
+		}
+
+
+		String getString(Context c) {
+			if(mMessage !=null) {
+				return mMessage;
+			} else {
+				if(mArgs == null)
+					return c.getString(mRessourceId);
+				else
+					return c.getString(mRessourceId,mArgs);
+			}
+		}
+	}
+	
+	private static final int MAXLOGENTRIES = 200;
+
+
+	public static final String MANAGMENT_PREFIX = "M:";
+
+
+	
+
+
+
+	public interface LogListener {
+		void newLog(LogItem logItem);
+	}
+	
+	public interface StateListener {
+		void updateState(String state, String logmessage);
+	}
+
+	synchronized static void logMessage(int level,String prefix, String message)
+	{
+		newlogItem(new LogItem(prefix +  message));
+
+	}
+
+	synchronized static void clearLog() {
+		logbuffer.clear();
+		logInformation();
+	}
+
+	private static void logInformation() {
+		
+		logInfo(R.string.mobile_info,Build.MODEL, Build.BOARD,Build.BRAND,Build.VERSION.SDK_INT);
+	}
+
+	synchronized static void addLogListener(LogListener ll){
+		logListener.add(ll);
+	}
+
+	synchronized static void removeLogListener(LogListener ll) {
+		logListener.remove(ll);
+	}
+
+	
+	synchronized static void addStateListener(StateListener sl){
+		stateListener.add(sl);
+		if(mLaststate!=null)
+			sl.updateState(mLaststate, mLaststatemsg);
+	}
+
+	synchronized static void removeSpeedListener(StateListener sl) {
+		stateListener.remove(sl);
+	}
+
+
+	synchronized public static LogItem[] getlogbuffer() {
+
+		// The stoned way of java to return an array from a vector
+		// brought to you by eclipse auto complete
+		return (LogItem[]) logbuffer.toArray(new LogItem[logbuffer.size()]);
+
+	}
+	public static void logBuilderConfig(String[] bconfig) {
+		mBconfig = bconfig;
+	}
+	public static void triggerLogBuilderConfig() {
+		if(mBconfig==null) {
+			logMessage(0, "", "No active interface");
+		} else {
+			for (String item : mBconfig) {
+				logMessage(0, "", item);
+			}	
+		}
+
+	}
+
+	public synchronized static void updateStateString(String state, String msg) {
+		mLaststate= state;
+		mLaststatemsg = msg;
+		
+		for (StateListener sl : stateListener) {
+			sl.updateState(state,msg);
+		}
+	}
+
+	public static void logInfo(String message) {
+		
+	}
+
+	public static void logInfo(int ressourceId, Object... args) {
+		newlogItem(new LogItem(LogItem.INFO, ressourceId, args));
+	}
+
+	private static void newlogItem(LogItem logItem) {
+		logbuffer.addLast(logItem);
+		if(logbuffer.size()>MAXLOGENTRIES)
+			logbuffer.removeFirst();
+		
+		for (LogListener ll : logListener) {
+			ll.newLog(logItem);
+		}
+	}
+
+	public static void logError(String msg) {
+		newlogItem(new LogItem(LogItem.ERROR, msg));
+		
+	}
+
+	public static void logError(int ressourceId) {
+		newlogItem(new LogItem(LogItem.ERROR, ressourceId));
+	}
+	public static void logError(int ressourceId, Object... args) {
+		newlogItem(new LogItem(LogItem.ERROR, ressourceId,args));
+	}
+	
+	
+}
